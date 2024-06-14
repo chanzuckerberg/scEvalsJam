51a069efe4089971a74754e63f391ae76a9ac447
kl11@sanger.ac.uk <kl11@sanger.ac.uk>
2024-06-14 15:16:58 +0000

    make scgen iterable over all perturbations

--- methods/notebooks/test_scgen_wrapper.ipynb.py
+++ methods/notebooks/test_scgen_wrapper.ipynb.py
@@ -16,6 +16,7 @@
 # COMMAND ----------
 
 # Install perturbench
+import os
 os.chdir('/Workspace/Users/lm25@sanger.ac.uk/scEvalsJam/perturbench')
 !pip install -e .
 
@@ -44,62 +45,61 @@ from perturbench.models import scgen as scgen_perturbench
 # COMMAND ----------
 
 # Load and prepare anndata
+adata_dict = {}
 adata = ad.read_h5ad('/dbfs/scEvalsJam/1gene-norman-split.h5ad')
-adata_filtered = adata[adata.obs['perturbation_name'].isin(['control','TSC22D1'])]
-
-adata_filtered.obs.groupby(['split'])['perturbation_name'].value_counts(normalize = True)
-
-
-# COMMAND ----------
-
 # Log-normalised counts (were log-tranformed before)
-adata_filtered.layers['counts'] = np.expm1(adata_filtered.X)
-adata_filtered.X = adata_filtered.layers['counts'].copy()
-sc.pp.normalize_total(adata_filtered, target_sum = 10000)
-sc.pp.log1p(adata_filtered)
+adata.layers['counts'] = np.expm1(adata.X)
+adata.X = adata.layers['counts'].copy()
+sc.pp.normalize_total(adata, target_sum = 10000)
+sc.pp.log1p(adata)
 
 # Create cell type column
-adata_filtered.obs['cell_type'] = 'cell_line'
+adata.obs['cell_type'] = 'cell_line'
 
 # COMMAND ----------
 
-# Split into train and test set
-train_idx, test_idx = train_test_split(adata_filtered.obs_names, train_size = 0.8, test_size = 0.2, stratify = adata_filtered.obs['perturbation_name'].tolist())
-adata_filtered.obs['train_or_test'] = ['train' if n in train_idx.tolist() else 'test' for n in adata_filtered.obs_names]
+#create a dictionnary
+adata_train_dict = {}
+adata_test_dict = {}
+for perturbation in adata.obs['perturbation_name'].unique():
+    if perturbation != 'control':
+        adata_filtered = adata[adata.obs['perturbation_name'].isin(['control',perturbation])].copy()
 
-adata_train = adata_filtered[~((adata_filtered.obs['train_or_test'] == 'test') & (adata_filtered.obs['perturbation_name'] == 'TSC22D1'))].copy()
-adata_truth = adata_filtered[~adata_filtered.obs_names.isin(adata_train.obs_names)].copy()
+        # Split into train and test set
+        train_idx, test_idx = train_test_split(adata_filtered.obs_names, train_size = 0.8, test_size = 0.2, stratify = adata_filtered.obs['perturbation_name'].tolist())
+        adata_filtered.obs['train_or_test'] = ['train' if n in train_idx.tolist() else 'test' for n in adata_filtered.obs_names]
+
+        adata_train = adata_filtered[~((adata_filtered.obs['train_or_test'] == 'test') & (adata_filtered.obs['perturbation_name'] == perturbation))].copy()
+        adata_truth = adata_filtered[~adata_filtered.obs_names.isin(adata_train.obs_names)].copy()
+        adata_train_dict[perturbation] = adata_train
+        adata_test_dict[perturbation] = adata_truth
 
 # COMMAND ----------
 
-adata_train.obs.groupby(['perturbation_name', 'train_or_test']).size()
 
-# COMMAND ----------
 
-model_params = {
-    'train_set':adata_train,
-    'batch_key':'perturbation_name',
-    'labels_key':'train_or_test',
-    'ctrl_key':'control',
-    'stim_key':'TSC22D1',
-    'cond_to_predict':'test',
-    'device':'cuda',
-    'save_model_path': '../saved_models/model_perturbation_prediction.pt',
-}
-model = scgen_perturbench.ScGenModel(**model_params)
 
 
 # COMMAND ----------
 
-model.train()
-
-# COMMAND ----------
-
-pred,delta = model.predict()
-
-# COMMAND ----------
-
-pred.X
+result_pred = []
+result_delta = []
+for perturbation, adata_train in adata_train_dict.items():
+    model_params = {
+        'train_set':adata_train,
+        'batch_key':'perturbation_name',
+        'labels_key':'train_or_test',
+        'ctrl_key':'control',
+        'stim_key':perturbation,
+        'cond_to_predict':'test',
+        'device':'cuda',
+        'save_model_path': '../saved_models/model_perturbation_prediction.pt',
+    }
+    model = scgen_perturbench.ScGenModel(**model_params)
+    model.train()
+    pred,delta = model.predict()
+    result_pred.append(pred)
+    result_delta.append(delta)
 
 # COMMAND ----------
 
